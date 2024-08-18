6170ba6dc6f915ce3d5159f376fb3554372095de
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-08-18 16:15:51 +0200

    use utils functions in reduct example

--- scripts/reduct_example.py
+++ scripts/reduct_example.py
@@ -2,22 +2,21 @@
 
 import asyncio
 import struct
-import time
 
 import numpy as np
 from reduct import Bucket, Client
-from utils import calculate_metrics, generate_sensor_data, pack_data
+from utils import (
+    calculate_metrics,
+    generate_sensor_data,
+    pack_data,
+    get_current_time,
+    TimeUnits,
+)
 
 
 HIGH_RMS = 1.0
 HIGH_CREST_FACTOR = 3.0
 HIGH_PEAK_TO_PEAK = 5.0
-
-
-async def setup_reductstore() -> Bucket:
-    """Set up the ReductStore client and create a bucket."""
-    client = Client("http://localhost:8383", api_token="my-token")
-    return await client.create_bucket("sensor_data", exist_ok=True)
 
 
 async def store_data(
@@ -37,42 +36,49 @@ async def store_data(
     await bucket.write("sensor_readings", packed_data, timestamp, labels=labels)
 
 
-async def query_data(bucket: Bucket, start_time: int, end_time: int):
+async def query_data(bucket: Bucket, start_time: int, end_time: int) -> np.ndarray:
     """Query and retrieve the stored data."""
+    query_signal = np.array([], dtype=np.float32)
     async for record in bucket.query(
         "sensor_readings", start=start_time, stop=end_time
     ):
-        print(f"Timestamp: {record.timestamp}")
-        print(f"Labels: {record.labels}")
-
         data = await record.read_all()
         num_points = len(data) // 4
         fmt = f">{num_points}f"
         signal = struct.unpack(fmt, data)
         signal = np.array(signal, dtype=np.float32)
+        query_signal = np.concatenate((query_signal, signal))
 
-        print(f"Number of data points: {num_points}")
-        print(f"First few values: {signal[:5]}")
-        print("---")
+    return query_signal
 
 
 async def main():
     """Example of storing and querying sensor data in ReductStore."""
-    bucket = await setup_reductstore()
-    start_time = int(time.time() * 1_000_000)
+    frequency = 1000
+    duration = 5
 
-    for _ in range(5):
-        timestamp = int(time.time() * 1_000_000)
-        signal = generate_sensor_data(frequency=1000, duration=1)
-        rms, peak_to_peak, crest_factor = calculate_metrics(signal)
-        packed_data = pack_data(signal)
-        await store_data(
-            bucket, timestamp, packed_data, rms, peak_to_peak, crest_factor
-        )
-        await asyncio.sleep(1)
+    async with Client("http://localhost:8383", api_token="my-token") as client:
+        bucket = await client.create_bucket("sensor_data", exist_ok=True)
+        start_time = get_current_time()
 
-    end_time = int(time.time() * 1_000_000)
-    await query_data(bucket, start_time, end_time)
+        signal = generate_sensor_data(frequency=frequency, duration=duration)
+        signal_chunks = np.array_split(signal, duration)
+
+        timestamp = get_current_time()
+        for chunk in signal_chunks:
+            rms, peak_to_peak, crest_factor = calculate_metrics(chunk)
+            packed_data = pack_data(chunk)
+            await store_data(
+                bucket, timestamp, packed_data, rms, peak_to_peak, crest_factor
+            )
+            await asyncio.sleep(1)
+            timestamp += len(chunk) * TimeUnits.MICROSECOND // frequency
+
+        end_time = get_current_time()
+        result = await query_data(bucket, start_time, end_time)
+        assert np.array_equal(
+            signal, result
+        ), f"Stored and queried data do not match: {len(signal)} vs {len(result)}"
 
 
 if __name__ == "__main__":
